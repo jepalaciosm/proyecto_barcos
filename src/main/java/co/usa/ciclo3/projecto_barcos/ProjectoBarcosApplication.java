423c9fa1fd93b086425efc5037335e4a1789a7ac
John Palacios <johnpalacios23@gmail.com>
2022-10-09 13:40:54 -0500

    Inicializamos proyecto con dependencias

---
+++ src/main/java/co/usa/ciclo3/projecto_barcos/ProjectoBarcosApplication.java
@@ -0,0 +1,13 @@
+package co.usa.ciclo3.projecto_barcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoBarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoBarcosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/usa/ciclo3/projecto_barcos/ProjectoBarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.usa.ciclo3.projecto_barcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoBarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
