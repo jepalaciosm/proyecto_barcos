0eeb515cb38262f398aea1f5ce615d53417e7b33
John Palacios <johnpalacios23@gmail.com>
2022-10-16 09:19:43 -0500

    agregamos mensages y reservations a boat

--- src/main/java/co/usa/ciclo3/projecto_barcos/model/Boat.java
+++ src/main/java/co/usa/ciclo3/projecto_barcos/model/Boat.java
@@ -7,6 +7,8 @@ package co.usa.ciclo3.projecto_barcos.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,6 +16,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -26,16 +29,39 @@ public class Boat implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String name;
     private String brand;
     @Column(name="years", nullable=false)
     private Integer year;
+    private String description; 
     @ManyToOne
     @JoinColumn(name="categoryId")
     @JsonIgnoreProperties("boats")
     private Category category;
-    private String name;        
-    private String description;    
+            
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "boat")
+    @JsonIgnoreProperties({"boat","client"})
+    private List<Message> messages;   
    
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "tool")
+    @JsonIgnoreProperties({"boat","messages"})
+    private List<Reservation> reservations;
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
     
     
 
